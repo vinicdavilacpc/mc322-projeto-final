d0078c07616c9ecdb8490a7121d9bb3c317d72c0
Isabella Favaron Rover <i281248@dac.unicamp.br>
2026-06-28 23:12:08 -0300

    cria a classe DataManager.java que implementa a interface Persistable. implementa os métodos save e add, e cria o método auxiliar jsonToList

---
+++ backend/src/main/java/com/agendajava/backend/model/DataManager.java
@@ -0,0 +1,70 @@
+package com.agendajava.backend.model;
+
+import com.agendajava.backend.interfaces.Persistable;
+
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.type.CollectionType;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class DataManager implements Persistable {
+    private final String USERS_FILE = "users.json";
+    private final String PROCEDURES_FILE = "procedures.json";
+    private final String ROOMS_FILE = "rooms.json";
+    private final ObjectMapper objectMapper;
+
+    public DataManager() {
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public String getUsersFile() {
+        return this.USERS_FILE;
+    }
+
+    public String getProceduresFile() {
+        return this.PROCEDURES_FILE;
+    }
+
+    public String getRoomsFile() {
+        return this.ROOMS_FILE;
+    }
+
+    private <T> List<T> jsonToList(String fileName, Class<T> type) {
+        File file = new File(fileName);
+
+        // se o json estiver vazio, retornamos uma lista vazia
+        if (!file.exists() || file.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        // se o json tiver informações, transformamos ele em uma lista para auxiliar na manipulação dele
+        CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, type);
+        return objectMapper.readValue(file, listType);
+    }
+
+    public <T> void save(String fileName, List<T> data) {
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(fileName), data);
+    }
+
+    public <T> void add(String fileName, T object) {
+        List<T> list = jsonToList(fileName, (Class<T>) object.getClass());
+        list.add(object);
+        save(fileName, list);
+    }
+
+    public <T> void delete(String fileName, T object) {
+        List<T> list = jsonToList(fileName, (Class<T>) object.getClass());
+
+        
+    }
+
+    public <T> T findOne(String fileName, Class<T> type, Predicate<T> filter) {
+        List<T> list = jsonToList(fileName, type);
+
+        
+    }
+
+}
