6abf1dce8dc34e7f05da4a9c90e74e5c27a307d2
Isabella Favaron Rover <i281248@dac.unicamp.br>
2026-06-29 14:11:12 -0300

    implementa os métodos da interface Authenticable na classe Authenticator. cria o método de login(), register() e userExists()

--- backend/src/main/java/com/agendajava/backend/interfaces/Authenticable.java
+++ backend/src/main/java/com/agendajava/backend/interfaces/Authenticable.java
@@ -6,7 +6,7 @@ import com.agendajava.backend.model.users.User;
 public interface Authenticable {
     public User login(String email, String password, DataManager dataManager);
 
-    public User register(String name, String password, String role, DataManager dataManager);
+    public User register(String name, String email, String password, String role, DataManager dataManager);
 
     public boolean userExists(String email, DataManager dataManager);
 }
