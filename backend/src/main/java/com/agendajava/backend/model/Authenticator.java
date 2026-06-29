5fd91d03b8d8b29287ff85e2f7cb8cb59d56238e
Isabella Favaron Rover <i281248@dac.unicamp.br>
2026-06-29 13:38:05 -0300

    cria a interface Authenticable e adiciona os cabeçalhos dos métodos. cria a classe Authenticator que implementa a interface

---
+++ backend/src/main/java/com/agendajava/backend/model/Authenticator.java
@@ -0,0 +1,21 @@
+package com.agendajava.backend.model;
+
+import com.agendajava.backend.interfaces.Authenticable;
+import com.agendajava.backend.model.users.User;
+
+public class Authenticator implements Authenticable {
+    // public User login(String email, String password, DataManager dataManager) {
+
+    // }
+
+    // public User register(String name, String password, String role, DataManager dataManager) {
+
+    // }
+
+    // public boolean userExists(String email, DataManager dataManager) {
+    //     User user = dataManager.findOne(
+    //         dataManager.getUsersFile(), 
+    //         User.class, 
+    //         u -> u.getEmail().equals(email));
+    // }
+}
