fd697bd56f317c2c0012e59c3cf35140c1bd357e
zenite06 <t278914@dac.unicamp.br>
2026-07-15 15:46:47 -0300

    Arquivos que faltaram do merge

---
+++ src/main/java/com/agendajava/backend/model/TimeBlock.java
@@ -0,0 +1,33 @@
+package main.java.com.agendajava.backend.model;
+
+import java.time.LocalTime;
+import java.time.DayOfWeek;
+
+/***
+ * Classe que representa um bloco de horário para o agendamento de cirurgias. 
+ * Contém um dia da semana, horário de início e horário de término.
+ * TimeBlock
+ */
+public class TimeBlock {
+    private DayOfWeek dayOfWeek;
+    private LocalTime start;
+    private LocalTime end;
+
+    public TimeBlock (DayOfWeek dayOfWeek, LocalTime start, LocalTime end) {
+        this.dayOfWeek = dayOfWeek;
+        this.start = start;
+        this.end = end;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+}
